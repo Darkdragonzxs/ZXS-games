30b87b24f46dde5dea851af330649c1fda9b134f
ZXS <DonaldBorthwick125@gmail.com>
2025-05-11 18:16:53 -0700

    Update uv.config.js

--- s/internet/uv/uv.config.js
+++ s/internet/uv/uv.config.js
@@ -1,14 +1,4 @@
 self.__uv$config = {
   prefix: '/s/internet/',
-  bare: 'https://t.thecappuccino.site', 
-  encodeUrl: function(input) {
-    try {
-      const url = new URL(input);
-      return this.prefix + this.encode(url.href);
-    } catch (e) {
-      return this.prefix + this.encode('http://' + input);
-    }
-  },
-  encode: (str) => btoa(unescape(encodeURIComponent(str))),
-  decode: (str) => decodeURIComponent(escape(atob(str))),
+  bare: 'https://t.thecappuccino.site',
 };
