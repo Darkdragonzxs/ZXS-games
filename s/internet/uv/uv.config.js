260b2471dd1ccf6d941d31976a5b930c428087a8
ZXS <DonaldBorthwick125@gmail.com>
2025-05-12 23:17:51 -0700

    Delete s/internet/uv/uv.config.js

--- s/internet/uv/uv.config.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', () => {
-  self.__uv$config = {
-    prefix: '/s/internet/',
-    bare: 'https://t.thecappuccino.site',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-  };
-});
