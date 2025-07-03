89254dc5d2269d32119020735729f72612ad56a9
ZXS <DonaldBorthwick125@gmail.com>
2025-07-02 19:39:34 -0700

    Delete s directory

--- s/internet/uv/uv.config.js
+++
@@ -1,6 +0,0 @@
-self.__uv$config = {
-  prefix: '/service/',
-  bare: 'https://api.bareserver.xyz', // CHANGE THIS
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-};
