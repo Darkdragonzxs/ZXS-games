b014073ca4c9154fc65cb4e7c1685eeda8ddff57
ZXS <DonaldBorthwick125@gmail.com>
2025-07-02 19:29:32 -0700

    Create uv.config.js

---
+++ s/internet/uv/uv.config.js
@@ -0,0 +1,6 @@
+self.__uv$config = {
+  prefix: '/service/',
+  bare: 'https://api.bareserver.xyz', // CHANGE THIS
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+};
