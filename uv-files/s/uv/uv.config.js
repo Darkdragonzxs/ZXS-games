7a41123190ac2c2c5f6ad66624ab2c33883f5ecd
ZXS <DonaldBorthwick125@gmail.com>
2025-07-10 15:53:48 -0700

    Update uv.config.js

--- uv-files/s/uv/uv.config.js
+++ uv-files/s/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
 	prefix: "/s/kibidi/",
-	bare: "https://nodiddydogwasatthediddyparty.global.ssl.fastly.net/bare/",
+	bare: "https://nodiddydogwasatthediddyparty.global.ssl.fastly.net/tspmo/",
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: "/s/uv/uv.handler.js",
