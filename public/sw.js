01372ad1d3d37ff9da182468c25d0fe0d97bea8c
ZXS <DonaldBorthwick125@gmail.com>
2025-09-10 13:32:53 -0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -5,26 +5,35 @@ if (navigator.userAgent.includes('Firefox')) {
     });
 }
 
-importScripts("/public/scram/scramjet.shared.js", "/public/scram/scramjet.worker.js");
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv/uv.config.js');
+importScripts('/uv/uv.sw.js');
+importScripts("/scram/scramjet.shared.js", "/scram/scramjet.worker.js");
 
+
+const uv = new UVServiceWorker();
 const scramjet = new ScramjetServiceWorker();
 
 let playgroundData;
 
-self.addEventListener("message", ({ data }) => {
-    if (data.type === "playgroundData") {
+self.addEventListener('message', ({ data }) => {
+    if (data.type === 'playgroundData') {
         playgroundData = data;
     }
 });
 
 async function handleRequest(event) {
+    if (uv.route(event)) {
+        return await uv.fetch(event);
+    }
+
     await scramjet.loadConfig();
     if (scramjet.route(event)) {
         return scramjet.fetch(event);
     }
-    return fetch(event.request);
+    return await fetch(event.request);
 }
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(handleRequest(event));
 });
