e233329a53acfc90202201c3bfb143d99527611b
ZXS <DonaldBorthwick125@gmail.com>
2025-09-12 11:29:19 -0500

    Update lethal.mjs

--- lethal.mjs
+++ lethal.mjs
@@ -1,8 +1,3 @@
-// Ultraviolet
-await import('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet/dist/uv.bundle.js');
-// UV Config
-await import('./uv.config.js');
-// Bare Mux
 import * as BareMux from 'https://cdn.jsdelivr.net/npm/@mercuryworkshop/bare-mux/dist/index.mjs';
 
 const connection = new BareMux.BareMuxConnection("/bareworker.js");
@@ -10,7 +5,6 @@ const connection = new BareMux.BareMuxConnection("/bareworker.js");
 let wispURL = null; // Not exported because it needs to be set through `setWisp`
 let transportURL = null; // Not exported because it needs to be set through `setTransport`
 
-// Service Worker for Ultraviolet
 const stockSW = "./ultraworker.js";
 const swAllowedHostnames = ["localhost", "127.0.0.1"];
 async function registerSW() {
